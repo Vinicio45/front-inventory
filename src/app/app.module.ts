e6a09e64c870220c6d3862acab4555df22d4051f
Allan Mendez <allanviniciom@gmail.com>
2024-07-02 17:30:51 -0600

    sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './modules/dashboard/pages/dashboard.component';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { DashboardModule } from './modules/dashboard/dashboard.module';
     AppRoutingModule,
     DashboardModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
